f7650c196e9509b32e2f0696a84993033c77244f
Célio Benjamim <celiolimabenjamim@gmail.com>
2025-11-04 22:40:51 -0400

    mudança no componente do input para atender melhor ao protótipo

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,7 +1,49 @@
-import { TextInput, TextInputProps } from "react-native";
+import { ComponentProps, useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TouchableOpacity, TextInput, TextInputProps, View } from "react-native";
 
 import { styles } from "./styles";
 
-export function Input({ ...rest }:TextInputProps) {
-    return <TextInput style={styles.input} {...rest} />   
+export type InputProps = TextInputProps & {
+    icon?: ComponentProps<typeof MaterialCommunityIcons>["name"]
+    size?: number
+    color?: string
+    isPsw?: boolean
+}
+
+export function Input({ icon, size, color, isPsw, ...rest }: InputProps) {
+    const [secure, setSecure] = useState(isPsw);
+
+    return (
+        <View style={[styles.container, { position: "relative" }]}>
+            <TextInput
+                style={[styles.input, { paddingLeft: icon ? (size ?? 24) + 16 : 12 }]}
+                placeholderTextColor="#7e7e99"
+                secureTextEntry={secure}
+                {...rest}
+            />
+            {icon && (
+                <MaterialCommunityIcons
+                    name={icon}
+                    size={size ?? 24}
+                    color={color ?? "#7e7e99"}
+                    style={{ position: "absolute", left: 12, top: 14 }}
+                />
+            )}
+
+            {isPsw && (
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    onPress={() => setSecure((s) => !s)}
+                    style={{ position: "absolute", right: 12, top: 14 }}
+                >
+                    <MaterialCommunityIcons
+                        name={secure ? "eye-off" : "eye"}
+                        size={size ?? 24}
+                        color={color ?? "#7e7e99"}
+                    />
+                </TouchableOpacity>
+            )}
+        </View>
+    )
 }
--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,13 +1,22 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+    },
     input: {
         width: "100%",
         height: 52,
         borderRadius: 10,
-        borderWidth: 1,
-        borderColor: "#121212",
+        borderWidth: 2,
+        borderColor: "#4A4E69",
         padding: 12,
-        fontSize: 16
-    }
+        fontSize: 16,
+        fontFamily: "Roboto_400Regular",
+        color: "#4A4E69",
+    },
 })
